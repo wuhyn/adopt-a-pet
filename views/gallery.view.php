345ec0a107ae075616869e03ec9014035c23b808
Hong Ng <hong.yang.ng@gmail.com>
2018-07-31 02:10:12 +1000

    Gallery database updates

--- views/gallery.view.php
+++ views/gallery.view.php
@@ -1,14 +1,20 @@
 <?php require('partials/head.php') ?>
+
+  <h1 class="text-center gallery-title">Explore Images</h1>
   <div class="container">
     <div class="row">
       <?php foreach ($images as $image) : ?>
           <div class="col-lg-4 col-sm-6">
+              <div class="single-image">
               <img class="img-thumbnail" src="<?= $image->image_path; ?>"  />
+              <p class="text-center">By: <?= $image->artist_name; ?></p>
+              </div>
           </div>
+
       <?php endforeach; ?>
 
     </div>
-  </div>  
+  </div>
 
 
 <?php require('partials/footer.php');

1a9e48941fa0d8369e5dba1cf0bef269fd8f5cc5
Hong Ng <hong.yang.ng@gmail.com>
2018-07-31 14:29:25 +1000

    Edit home page text

--- views/index.view.php
+++ views/index.view.php
@@ -5,9 +5,9 @@
         <h1 class="display-4">The Image Gallery</h1>
         <p class="lead">Share and showcase photography and art works.</p>
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>Upload your pictures.</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
     </div>
-    
+
 <?php require('partials/footer.php');
